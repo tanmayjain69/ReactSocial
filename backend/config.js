9fa0b0768bd7ffbe54e380da6731061ec0f3f940
tanmayjain69 <36137197+tanmayjain69@users.noreply.github.com>
2019-05-26 11:01:30 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,12 +1,12 @@
 export const facebook = {
-  clientID: '828171574217396',
-  clientSecret: '02e511ad28d627a0d89bdbcee5acfde2',
+  clientID: 'YOUR ID',
+  clientSecret: 'YOUR KEY',
   callbackURL: 'http://localhost:3000/auth/facebook/callback',
   profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
 };
 
 export const google = {
-  clientID: '820301842225-nfmud2m6oou0nnd3i4dufph99cj2b3b3.apps.googleusercontent.com',
-  clientSecret: 'PaemBJUVr4DtKD0i-Wdt6EiO',
+  clientID: 'YOUR ID',
+  clientSecret: 'YOUR KEY',
   callbackURL: 'http://localhost:3000/auth/google/callback',
 };
